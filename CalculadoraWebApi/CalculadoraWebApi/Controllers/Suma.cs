947d6c904fc2607a3b8820503d96baeba9da17b0
Hernandez <david.2.hernandez@atos.net>
2022-07-06 16:57:50 -0500

    Web api con endpoints de la calculadora

---
+++ CalculadoraWebApi/CalculadoraWebApi/Controllers/Suma.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Suma : Controller
+    {
+        // GET
+        [HttpGet("{a}/{b}")]
+        public double Get(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
